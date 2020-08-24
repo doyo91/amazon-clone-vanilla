a3901bff591df0c43036e8f8f7947f431db0ff75
doyo91 <doyo_91@hotmail.com>
2020-08-24 11:56:42 +0200

    Add to cart action

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,38 @@
+import { parseRequestUrl } from "../helpers/utils";
+import { getProduct } from "../services/api";
+import { getCartItems, setCartItems } from "../services/localStorage";
+
+const addToCart = (item, forceUpdate = false) => {
+  let cartItems = getCartItems();
+  const existItem = cartItems.find((x) => x.product === item.product);
+  if (existItem) {
+    cartItems = cartItems.map((x) =>
+      x.product === existItem.product ? item : x
+    );
+  } else {
+    cartItems = [...cartItems, item];
+  }
+  setCartItems(cartItems);
+};
+
+const CartScreen = {
+  after_render: () => {},
+  render: async () => {
+    const request = parseRequestUrl();
+    if (request.id) {
+      const product = await getProduct(request.id);
+      addToCart({
+        product: product._id,
+        name: product.name,
+        image: product.image,
+        price: product.price,
+        countInStock: product.countInStock,
+        qty: 1,
+      });
+    }
+    return `<div>CartScreen</div>
+    <div>${getCartItems().length}</div>`;
+  },
+};
+
+export default CartScreen;
