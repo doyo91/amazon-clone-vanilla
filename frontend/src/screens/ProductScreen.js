9c2fd27397559cb534e7123d7d9441cb27cdd9a3
doyo91 <doyo_91@hotmail.com>
2020-08-24 11:23:20 +0200

    UI productScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { parseRequestUrl } from "../helpers/utils";
 import { getProduct } from "../services/api";
+import Rating from "../components/Rating";
 
 const ProductScreen = {
   render: async () => {
@@ -8,7 +9,51 @@ const ProductScreen = {
     if (product.error) {
       return `<div>${product.error}</div>`;
     }
-    return `<h1>${product.name}</h1>`;
+    return `
+    <div class="content-product">
+      <div class="back-to-result">
+        <a href="/#/">Back to result </a>
+      </div>
+      <div class="product-details">
+    <div class="product-details__image">
+        <img src="${product.image}" alt="${product.name}">
+    </div>
+    <div class="details-info">
+        <h1 class="details-info__title">
+            ${product.brand} ${product.name}
+        </h1>
+        <div class="details-info__rating">
+            ${Rating.render({
+              value: product.rating,
+              text: `${product.numReviews} reviews`,
+            })}
+        </div>
+        <p class="details-info__price">
+            Price: <strong>${product.price}<span>€</span></strong>
+        </p>
+        <div class="details-info__description">
+            Description:
+            <p>
+                ${product.description}
+            </p>
+        </div>
+    </div>
+    <div class="details-action">
+        <ul>
+            <li><strong>${product.price}<span>€</span></strong></li>
+            <li>Status: ${
+              product.countInStock > 0
+                ? `<span class="details-action__success">In Stock</span>`
+                : `<span class="details-action__error">Unavailable</span>`
+            }</li>
+            <li>
+                <button id="add-btn" class="details-action__btn">Add to Cart</button>
+            </li>
+        </ul>
+    </div>
+</div>
+    </div>
+    `;
   },
 };
 
