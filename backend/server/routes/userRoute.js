5193dce47cd4b47106625a5ca37a10ff171d9f40
doyo91 <doyo_91@hotmail.com>
2020-08-25 11:53:01 +0200

    Signin backend

--- backend/server/routes/userRoute.js
+++ backend/server/routes/userRoute.js
@@ -1,21 +1,48 @@
 import express from "express";
 import User from "../models/userModel";
+import expressAsyncHandler from "express-async-handler";
+import { generateToken } from "../services/utils";
 
 const userRouter = express.Router();
 
-userRouter.get("/createadmin", async (req, res) => {
-  try {
-    const user = new User({
-      name: "admin",
-      email: "admin@example.com",
-      password: "123456",
-      isAdmin: true,
-    });
-    const createdUser = await user.save();
-    res.send(createdUser);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
+userRouter.get(
+  "/createadmin",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = new User({
+        name: "admin",
+        email: "admin@example.com",
+        password: "123456",
+        isAdmin: true,
+      });
+      const createdUser = await user.save();
+      res.send(createdUser);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  })
+);
 
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (!signinUser) {
+      res.status(401).send({
+        message: "Invalid email or password",
+      });
+    } else {
+      res.send({
+        _id: signinUser._id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: generateToken(signinUser),
+      });
+    }
+  })
+);
 export default userRouter;
